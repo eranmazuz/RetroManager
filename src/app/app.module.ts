4520fde4cefa31f006d6384ed746ecd60e30ab19
Eran Mazuz <mazuzeran@gmail.com>
2020-06-20 19:21:17 +0300

    Creating the components + site structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { GamesComponent } from './games/games.component';
+import { GameListComponent } from './games/game-list/game-list.component';
+import { GameDetailComponent } from './games/game-detail/game-detail.component';
+import { GameItemComponent } from './games/game-list/game-item/game-item.component';
+import { GameFilterComponent } from './games/game-filter/game-filter.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    GamesComponent,
+    GameListComponent,
+    GameDetailComponent,
+    GameItemComponent,
+    GameFilterComponent,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/games/game-filter/game-filter.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-game-filter',
+  templateUrl: './game-filter.component.html',
+  styleUrls: ['./game-filter.component.css']
+})
+export class GameFilterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
